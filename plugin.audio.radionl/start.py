03b419fef746809893c3aad6742cf05f1b76192d
Hades <79419919+Son-of-Cronus@users.noreply.github.com>
2021-09-04 02:22:27 +0200

    Update start.py
    Committed python 3 changes

--- plugin.audio.radionl/start.py
+++ plugin.audio.radionl/start.py
@@ -1,30 +1,42 @@
+'''
+    Stendersmusic Service Radio Add-on
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GPL-3.0 License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 import os
 import sys
-import urlparse
-import xbmc 
-import xbmcaddon
-
+import urllib.parse
+from kodi_six import xbmc, xbmcaddon, xbmcvfs
 from resources.lib import xbmcutil
 
-__addon__           = xbmcaddon.Addon()
-__author__          = __addon__.getAddonInfo('author')
-__addon_id__	= __addon__.getAddonInfo('id')
-__addon_name__      = __addon__.getAddonInfo('name')
+__addon__               = xbmcaddon.Addon()
+__author__              = __addon__.getAddonInfo('author')
+__addon_id__		= __addon__.getAddonInfo('id')
+__addon_name__ 	        = __addon__.getAddonInfo('name')
 __addon_path__	   	= __addon__.getAddonInfo('path')
 __addon_version__	= __addon__.getAddonInfo('version')
 __addon_fanart__	= __addon__.getAddonInfo('fanart')
-__addon_icon__      = __addon__.getAddonInfo('icon')
-__country_code__    = 'NL'
+__addon_icon__		= __addon__.getAddonInfo('icon')
+__country_code__	= 'NL'
 
-__profile__         = xbmc.translatePath(__addon__.getAddonInfo('profile'))
+__profile__             = xbmc.translatePath(__addon__.getAddonInfo('profile'))
 
-args = urlparse.parse_qs(sys.argv[2][1:])
+args = urllib.parse.parse_qs(sys.argv[2][1:])
 
 addon_handle=int(sys.argv[1])
 xbmcutil.addon_handle=addon_handle
 
-__settings__ = xbmcaddon.Addon(id='plugin.audio.radionl')
-rootDir = xbmc.translatePath(__settings__.getAddonInfo('path')).decode('utf-8')
+__settings__= xbmcaddon.Addon(id='plugin.audio.radionl')
+rootDir = xbmc.translatePath(__settings__.getAddonInfo('path'))
 streamDir = os.path.join(rootDir, "streams")
 
 def browse(strDir):
@@ -32,11 +44,11 @@ def browse(strDir):
         xbmcutil.addMenuItem('[COLOR orange]'+directory+'[/COLOR]', os.path.join(strDir, directory), 'false')
     for file in getFiles(strDir) :
         if(file[-5:] == '.strm') :
-            background = os.path.join(rootDir, 'fanart.jpg')
+            background = os.path.join(rootDir, 'resources/fanart.jpg')
             if(os.path.isfile(os.path.join(strDir, file[:-5]+'.tbn'))) :
                 iconFile = os.path.join(strDir, file[:-5]+'.tbn')
             else :
-                iconFile = os.path.join(rootDir, 'icon.png')
+                iconFile = os.path.join(rootDir, 'resources/logo.png')
             xbmcutil.addMenuItem(file[:-5], os.path.join(strDir, file), 'true', icon=iconFile, fanart=background)
     xbmcutil.endOfList()
 
